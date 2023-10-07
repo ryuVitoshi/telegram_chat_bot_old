72b527ca256990f5012069d3406a5ee66c5e6731
ryuVitoshi <ryu.vitoshi@gmail.com>
2023-10-07 17:50:57 +0300

    Second commit

--- bot.py
+++ bot.py
@@ -3,6 +3,8 @@ import telebot
 from telebot import types
 from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
 import credentials
+import texts
+import design
 
 bot = credentials.bot
 
@@ -25,17 +27,22 @@ def button_press_handler(call):
     elif data == '':
         pass
 
-# Welcoming message
+# Commands
+
+# Welcome message
 start_commands = ["start", "home"]
 @bot.message_handler(start_commands)
 def send_welcome(message):
-    print(message.from_user.id)
-    pass
+    uid = message.from_user.id
+    print(uid)
+    photo = open('images/welcome.jpg', 'rb')
+    #bot.send_photo(uid, photo, texts.WELCOME_TEXT, parse_mode='html', reply_markup = design.welcome_message_with_buttons())
+    bot.send_message(uid, photo, texts.KEYBOARD, parse_mode='html', reply_markup = design.keyboard()) 
 
 # HELP message
 start_commands = ["help"]
 @bot.message_handler(start_commands)
-def send_welcome(message):
+def send_help(message):
     print(message.from_user.id)
     pass
 
---
+++ texts.py
@@ -0,0 +1,59 @@
+#HELP
+desc = "This bot was created for online consultation of customers of our car maintance service.\nWith its help, you can view services, as well as make an appointment."
+coms = '''
+Bot commands:
+/start - display welcome message
+/help - provides information about bot
+/aboutus - information about our comany
+/services - provide informtion about our sevices and products 
+/profile - your contact information
+/question - share with manager your questions and suggestions
+
+You can also press the buttons on the keyboard below. 
+There you can find our services, information about us, your profile and a "Home" button that returns you to the initial message.
+'''
+HOW_HELP='How can we help you?\nPlease, chose an option bellow 👇'
+HELP_TEXT= f'Welcome!\n{desc}\n{coms}\n{HOW_HELP}'
+KEYBOARD = "Our services, contacts and your profile"
+
+#WELCOME
+WELCOME_TEXT='''🚙 Welcome to <b>Super Car Service!</b> 🚙
+
+✅ Our qualified team will help you <b>solve problems</b> with your car and provide <b>services such as:</b>
+
+   - Diagnostics (undercarriage and computer diagnostics); 📊
+   -	Car repair; 🛠️
+   -	Ordering auto parts; 🔩
+   -	Refueling the air conditioner; 🌀
+   -	Car painting and tuning; 🎨
+   -	Car polishing. ✨
+
+
+You can learn more about our services in the <b>«Services and products»</b> section below 👇
+
+🤷‍♀️ If you need help with bot, type "/help" or contact the manager "/question".
+'''
+
+# Company info
+name = "<b>Super Car Service</b>"
+desc = ""
+phone = "+3590888888"
+email ="supercarservice@hmail.com"
+location = "9000 Varna Center, Varna 43.209337, 27.911042"
+ppl ="Dimitriy Slaveykov"
+COMPANY_INFO = f'{name}\n{desc}\n📍{location}\n✉️ {email}\n📞 {phone} \t{ppl}'
+
+
+# Ask
+ASK = '''❓Got questions or suggestions? <b>Share with us</b>!
+
+🗣️Our managers will provide answers at <b>the first opportunity</b>!
+🗓️ Business hours are <b>Monday-Friday</b> from <b>9:00 a.m.</b> to <b>6:00 p.m</b>
+
+\nThank you for understanding ✅
+'''
+
+#DIAGNOSTIC
+DIAGNOSTICS = '''📊 <b>Diagnostics</b> 📊 
+
+Our service carries out two types of car diagnostics. You can learn more about each of them and sign up below 👇'''
