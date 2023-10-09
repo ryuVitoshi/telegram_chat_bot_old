a9c62ad98f4f0e8cf8626f188e301742583788c0
ryuVitoshi <ryu.vitoshi@gmail.com>
2023-10-10 00:52:49 +0300

    remove credentials

--- credentials.py
+++
@@ -1,6 +0,0 @@
-import telebot
-
-BOT_TOKEN = "815635409:AAFRAuEWYemuZOYQq9T1PdoU4qQv-lBnfbE"
-URL = "https://t.me/dnd_roll_dice_bot"
-bot = telebot.TeleBot(BOT_TOKEN)
-database = "superbarbershop.db"
